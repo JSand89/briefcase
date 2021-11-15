e8bb892e8622f28d512fe21cc6c34d54d5dbeec3
JSand89 <javier.esand@gmail.com>
2021-11-15 15:45:54 -0500

    little cha-cha-changees

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import {React, useState, useEffect} from 'react'
 import Data from '../../projects.json'
 import Project from '../Project/Project'
+import style from './projects.module.css'
 
 function Cards() {
     const [search,searchFunction]=useState('Search')
     function cond(e){
-        e.Name.includes(search)
-        console.log(e.Name.includes(search))
+        e.tecnologys.includes(search)
+        // console.log(e.Name.includes(search))
     }
 
     var Filter = search ==="Search"| search ===''? Data: Data.filter(item=>item.tecnologys.includes(search))
@@ -19,9 +20,13 @@ function Cards() {
     },[search])
     return (
         <>
-        <input type="search" placeholder={search} 
-        onChange= {Event=>searchFunction(Event.target.value)} ></input>
-        <div>
+            <h2 className={style.profile}>PROJECTS</h2>
+        <input type="search" 
+        placeholder={search} 
+        onChange= {Event=>searchFunction(Event.target.value)}
+        >
+        </input>
+        <div className={style.Cards}>
             {Filter.map(item=><Project props={item} key={item.id}/>)}
         </div>
         </>
