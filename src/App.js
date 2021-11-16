514bdb93f237472493071b5d56ededc00fc14fc2
JSand89 <javier.esand@gmail.com>
2021-11-16 07:16:49 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './components/header/Header';
 import Texts from './components/Texts/Texts';
 import Projects from './components/Projects/Projects'
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
+      
       <Header></Header>
       <Texts></Texts>
       <Projects></Projects>
+      <Footer></Footer>
     </div>
   );
 }
